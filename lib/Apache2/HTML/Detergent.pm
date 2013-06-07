b0b29ff5f25308aff4679f82ecb5ae9ac06fb762
Dorian Taylor <dorian.taylor.lists@gmail.com>
2013-06-07 15:44:52 -0700

    bug fixes

--- lib/Apache2/HTML/Detergent.pm
+++ lib/Apache2/HTML/Detergent.pm
@@ -43,11 +43,11 @@ Apache2::HTML::Detergent - Clean the gunk off HTML documents on the fly
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
