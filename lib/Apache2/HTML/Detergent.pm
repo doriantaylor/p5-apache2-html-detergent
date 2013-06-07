74e1fc77338d7c92518ce91ec4cef3a9ea446569
Dorian Taylor <dorian.taylor.lists@gmail.com>
2013-06-07 15:46:43 -0700

    version rev

--- lib/Apache2/HTML/Detergent.pm
+++ lib/Apache2/HTML/Detergent.pm
@@ -43,11 +43,11 @@ Apache2::HTML::Detergent - Clean the gunk off HTML documents on the fly
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
